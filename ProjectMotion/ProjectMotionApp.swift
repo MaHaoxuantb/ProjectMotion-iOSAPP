1ce21f9c4cb2a41c33ec245133a8623f22088be6
ThomasB <mahaoxuan_tb@outlook.com>
2025-09-25 20:05:59 +0800

    Initial Commit

---
+++ ProjectMotion/ProjectMotionApp.swift
@@ -0,0 +1,32 @@
+//
+//  ProjectMotionApp.swift
+//  ProjectMotion
+//
+//  Created by Thomas B on 9/25/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ProjectMotionApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
